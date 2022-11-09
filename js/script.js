5ee6b27e39e6c8660d5ec0a84abe4d854583e03a
mohamed <am8727244@gmail.com>
2022-11-09 22:45:18 +0100

    validation system handled by js but not completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const forms = document.querySelector(".forms"),
+    email1 = document.querySelector(".email1"),
+    email2 = document.querySelector(".email2"),
+    pwd1 = document.querySelector(".password"),
+    pwd2 = document.querySelector(".password2"),
     pwdShowHide = document.querySelectorAll(".eye-icon"),
     links = document.querySelectorAll(".link");
 
+
 pwdShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password , .password2");
 
         pwFields.forEach(password => {
             if(password.type === "password"){
@@ -24,3 +29,83 @@ links.forEach(link => {
         forms.classList.toggle("show-signup");
     })
 })
+
+
+// Handle the validation form
+forms.addEventListener("submit", e => {
+    e.preventDefault();
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+    errorDisplay.innerText = message;
+
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
+    errorDisplay.innerText = "";
+
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isValidEmail1 = email1 => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email1).toLowerCase());
+}
+const isValidEmail2 = email2 => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email2).toLowerCase());
+}
+
+const validateInputs = () => {
+    const emailValue1 = email1.value.trim();
+    const emailValue2 = email2.value.trim();
+    const pwdlValue1 = pwd1.value.trim();
+    const pwdlValue2 = pwd2.value.trim();
+
+    if(emailValue1 === "") {
+        setError(email1, 'Email is requierd!');
+    }
+    else if(!isValidEmail1(emailValue1)) {
+        setError(email1, 'Set a valid email address');
+    }
+    else{
+        setSuccess(email1);
+    }
+    if(emailValue2 === "") {
+        setError(email2, 'Email is requierd!');
+    }
+    else if(!isValidEmail2(emailValue2)) {
+        setError(email2, 'Set a valid email address');
+    }
+    else{
+        setSuccess(email2);
+    }
+
+
+    if(pwdlValue1 === "") {
+        setError(pwd1, 'Password is required!');
+    }
+    else if(pwdlValue1.length < 8) {
+        setError(pwd1, 'Password must be at least 8 characters.');
+    }
+    else{
+        setSuccess(pwd1);
+    }
+    if(pwdlValue2 === "") {
+        setError(pwd2, 'Please confirm your password!');
+    }
+    else if(pwdlValue1 !== pwdlValue2) {
+        setError(pwd2, "Passwords doesn't match");
+    }
+    else{
+        setSuccess(pwd2);
+    }
+}
